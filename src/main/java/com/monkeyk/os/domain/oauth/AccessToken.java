8a560a6187a4170a6859771532f6b15577e97686
Li Shengzhao <shengzhan@uwantsoft.com>
2015-09-29 12:13:46 +0800

    Remove unused comments

--- src/main/java/com/monkeyk/os/domain/oauth/AccessToken.java
+++ src/main/java/com/monkeyk/os/domain/oauth/AccessToken.java
@@ -1,14 +1,3 @@
-/*
- * Copyright (c) 2013 Andaily Information Technology Co. Ltd
- * www.andaily.com
- * All rights reserved.
- *
- * This software is the confidential and proprietary information of
- * Andaily Information Technology Co. Ltd ("Confidential Information").
- * You shall not disclose such Confidential Information and shall use
- * it only in accordance with the terms of the license agreement you
- * entered into with Andaily Information Technology Co. Ltd.
- */
 package com.monkeyk.os.domain.oauth;
 
 import com.monkeyk.os.domain.AbstractDomain;
