a4c131d7fabcdf935a6a6ed575b63bb582d2f125
LSZ <zhao@idsmanager.com>
2016-09-03 22:51:00 +0800

    (161) - 增加必要的代码注释与配置注释, 更易理解

--- core/src/main/java/com/monkeyk/os/domain/oauth/ClientDetails.java
+++ core/src/main/java/com/monkeyk/os/domain/oauth/ClientDetails.java
@@ -11,16 +11,26 @@ import java.util.Date;
  * 15-6-12
  * <p/>
  * DBTable: oauth_client_details
+ * <p/>
+ * 定义OAuth2 的客户端(client details)
  *
  * @author Shengzhao Li
  */
 public class ClientDetails extends BasicClientInfo {
 
 
+    /**
+     * 客户端所拥有的资源ID(resource-id), 至少有一个,
+     * 多个ID时使用逗号(,)分隔, 如:  os,mobile
+     */
     private String resourceIds;
 
     private String scope;
 
+    /**
+     * 客户端所支持的授权模式(grant_type),
+     * 至少一个, 多个值时使用逗号(,)分隔, 如: password,refresh_token
+     */
     private String grantTypes;
 
     /*
@@ -28,15 +38,34 @@ public class ClientDetails extends BasicClientInfo {
    * */
     private String roles;
 
+    /**
+     * access_token 的有效时长, 单位: 秒.
+     * 若不填或为空(null)则使用默认值: 12小时
+     */
     private Integer accessTokenValidity;
 
+    /**
+     * refresh_token的 有效时长, 单位: 秒
+     * 若不填或为空(null)则使用默认值: 30天
+     */
     private Integer refreshTokenValidity;
 
+    /**
+     * 该 客户端是否为授信任的,
+     * 若为信任的,, 则在 grant_type = authorization_code 时将跳过用户同意/授权 步骤
+     */
     private boolean trusted = false;
 
+    /**
+     * 逻辑删除的标识, true表示已经删除
+     */
     private boolean archived = false;
 
+    /**
+     * 创建日间
+     */
     private Date createTime = DateUtils.now();
+
 
     public ClientDetails() {
     }
