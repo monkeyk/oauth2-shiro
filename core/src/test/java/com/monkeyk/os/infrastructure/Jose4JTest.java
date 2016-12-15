87e13481033af0b80c0048de39ad84eb00b244fd
monkeyk7 <monkeyk1987@gmail.com>
2016-12-15 22:34:19 +0800

    Add Jose4JTest

--- core/src/test/java/com/monkeyk/os/infrastructure/Jose4JTest.java
+++ core/src/test/java/com/monkeyk/os/infrastructure/Jose4JTest.java
@@ -4,7 +4,6 @@ import org.jose4j.jwe.ContentEncryptionAlgorithmIdentifiers;
 import org.jose4j.jwe.JsonWebEncryption;
 import org.jose4j.jwe.KeyManagementAlgorithmIdentifiers;
 import org.jose4j.keys.AesKey;
-import org.jose4j.lang.ByteUtil;
 import org.testng.annotations.Test;
 
 import java.security.Key;
@@ -21,11 +20,10 @@ public class Jose4JTest {
 
 
     /*
-    * AES 加密与解密
+    * AES 加密与解密, 128位
     * */
-
     @Test
-    public void aesEncryptDecrypt() throws Exception {
+    public void aesEncryptDecrypt128() throws Exception {
 
         String keyText = "iue98623diDEs096";
         String data = "I am marico";
@@ -51,6 +49,38 @@ public class Jose4JTest {
         assertNotNull(payload);
         assertEquals(payload, data);
 
+    }
+
+
+    /*
+    * AES 加密与解密, 256位
+    * */
+    @Test
+    public void aesEncryptDecrypt256() throws Exception {
+
+        String keyText = "iue98623diDEs096_8u@idls(*JKse09";
+        String data = "I am marico";
+        Key key = new AesKey(keyText.getBytes());
+
+        //加密
+        JsonWebEncryption jwe = new JsonWebEncryption();
+        jwe.setAlgorithmHeaderValue(KeyManagementAlgorithmIdentifiers.A256KW);
+        jwe.setEncryptionMethodHeaderParameter(ContentEncryptionAlgorithmIdentifiers.AES_256_CBC_HMAC_SHA_512);
+        jwe.setKey(key);
+        jwe.setPayload(data);
+
+        String idToken = jwe.getCompactSerialization();
+        assertNotNull(idToken);
+        System.out.println(data + " idToken: " + idToken);
+
+        //解密
+        JsonWebEncryption jwe2 = new JsonWebEncryption();
+        jwe2.setKey(key);
+        jwe2.setCompactSerialization(idToken);
+
+        final String payload = jwe2.getPayload();
+        assertNotNull(payload);
+        assertEquals(payload, data);
 
     }
 
